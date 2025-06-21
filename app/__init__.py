edab60965f67d973088e29c61e803399b6eb3d72
Jia <77560326+Jxl-s@users.noreply.github.com>
2025-06-21 19:33:26 -0400

    feat: insert data for future template use

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,53 @@ from flask import Flask, render_template, request
 load_dotenv()
 app = Flask(__name__)
 
+experience = [
+    {
+        "title": "Production Engineering Fellow",
+        "company": "Meta & Major League Hacking",
+        "date": "June 2025 - Present",
+        "location": "Remote",
+        "description": [
+            "Deploying scalable infrastructure with Docker and Linux, and automating CI/CD pipelines with GitHub Actions."
+        ],
+    },
+    {
+        "title": "Software Development Intern",
+        "company": "NDT Technologies Inc.",
+        "date": "January 2024 - August 2024",
+        "location": "Montreal, Canada",
+        "description": [
+            "Cut page loading times from 30s to 0.15s by optimizing database queries, significantly enhancing user experience.",
+            "Reduced administration workload by 40% by developing a 4-level authentication flow, hardening system security.",
+        ],
+    },
+]
+
+education = [
+    {
+        "degree": "Bachelor of Science in Computer Science",
+        "school": "Concordia University",
+        "date": "Expected: June 2027",
+        "location": "Montreal, Canada",
+        "grade": "GPA: 4.17/4.3",
+    }
+]
+
+hobbies = [
+    {
+        "title": "3D Modeling",
+        "description": "Creating basic 3D models for fun using Blender.",
+    }
+]
+
 
 @app.route("/")
 def index():
-    return render_template("index.html", title="Jia Xuan Li", url=os.getenv("URL"))
+    return render_template(
+        "index.html",
+        title="Jia Xuan Li",
+        url=os.getenv("URL"),
+        experience=experience,
+        education=education,
+        hobbies=hobbies,
+    )
