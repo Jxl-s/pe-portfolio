1aae90cae5adc7140acd803598862fd86739d4a0
Jia <77560326+Jxl-s@users.noreply.github.com>
2025-07-07 13:48:26 -0400

    update redeploy

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-tmux kill-session -t flask
 git fetch && git reset origin/main --hard
 
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
 
-tmux new -d -s flask "flask run --host 0.0.0.0"
-echo "Re-deployed application"
+systemctl restart myportfolio
